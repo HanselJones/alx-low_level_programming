ede530e2e7e783965424c26ccf541c6267914007
Hansel Jones <hanseljonnes@gmail.com>
2022-03-15 15:20:19 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
+ *  main - entry point.
  *
- *  *main - entry point.
+ *  Description: program starts and ends here.
  *
- *   *Description: program starts and ends here.
+ *  Return: 0 on success.
  *
- *    *Return: 0 on success.
- *
- *     */
+ */
 
 int main(void)
 
