a7d218a690d02de538b204a450b747d3aa630875
Hansel Jones <hanseljonnes@gmail.com>
2022-03-21 10:59:36 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 	char t;
 	int i, x, y;
 
-	x = 0;|
+	x = 0;
 	y = 0;
 
 	while (s[x] != '\0')
@@ -21,7 +21,7 @@ void rev_string(char *s)
 
 	y = x - 1;
 
-	for (i = 0, i < x / 2; i++)
+	for (i = 0; i < x / 2; i++;)
 	{
 		t = s[i];
 		s[i] = s[y];
